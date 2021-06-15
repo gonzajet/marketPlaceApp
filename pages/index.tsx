761a761254080c9bd806884cc2cc555a355fdf4b
Gonzalo Rodriguez <gonzalo.rodriguez@southworks.com>
2021-06-14 21:11:38 -0300

    Improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,16 @@ import type { AppProps } from "next/app";
 import Link from "next/link";
 import { NavigationPath } from "models/enum";
 
-
-function Index({ Component, pageProps }: AppProps): JSX.Element {
-    return (
-     <>
-      <h1 className="title">
-          Go to{" "}
-          <Link href={NavigationPath.home}>
-            <a>Home!</a>
-          </Link>
-        </h1>
-     </>
-    );
-  }
-  export default Index;
-
+function Index(): JSX.Element {
+  return (
+    <>
+      <h1>
+        Go to{" "}
+        <Link href={NavigationPath.home}>
+          <a>Home!</a>
+        </Link>
+      </h1>
+    </>
+  );
+}
+export default Index;
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,6 +1,6 @@
 import { ProductCard } from "./components/productCard";
 
-export default function Product() {
+export default function Product(): JSX.Element {
   const displayCards = (): JSX.Element => {
     var cards = [];
     for (var i = 0; i < 10; i++) {
